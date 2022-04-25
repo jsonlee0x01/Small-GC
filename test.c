bb211fe7ee6126f8a7e091ff7f69caee3601fd0d
Json Lee <87839054+jsonlee0x01@users.noreply.github.com>
2022-04-25 19:47:22 +0800

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,20 @@
+/*
+Execution logs:
+
+Heap size = 239
+Heap size = 600002
+Heap size = 600002
+Heap size = 600002
+Heap size = 600002
+Heap size = 600002
+Heap size = 600002
+Heap size = 600002
+Heap size = 600002
+Heap size = 600002
+Heap size = 2
+Heap size = 2
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
